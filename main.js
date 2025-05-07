db9f88b42f31cdaf863d25517fb17c5bbaae8f18
florentinebusch <florentine.busch@student.uibc.ac.at>
2025-05-07 14:14:24 +0200

    Popup implementiert und Namen hinzugefügt

--- main.js
+++ main.js
@@ -39,6 +39,21 @@ async function loadStations(url) {
     let jsondata = await response.json();
 
     // Wetterstationen mit Icons und Popups
+    L.geoJSON(jsondata, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: "icons/wifi.png",
+                })
+            });
+        },
+        onEachFeature: function (feature, layer) {
+            console.log(feature.properties)
+            layer.bindPopup(`
+                <h4>${feature.properties.name}</h4>
+                `);
+        }
+    }).addTo(overlays.stations);
 
 }
 loadStations("https://static.avalanche.report/weather_stations/stations.geojson");
